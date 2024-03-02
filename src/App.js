b7aa34df17752fb554a5e45de2d5052831eff093
L1MBeR1 <paol670@mail.ru>
2024-03-02 17:42:38 +0800

    Node test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactFlow from 'reactflow';
+import React, { useCallback } from 'react';
+import ReactFlow, {
+    MiniMap,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+  } from 'reactflow';
+ 
 import 'reactflow/dist/style.css';
-
+ 
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
-
-export default function App() {
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  ];
+  const initialEdges = [];
+   
+  export default function App() {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+   
+    const onConnect = useCallback(
+      (params) => setEdges((eds) => addEdge(params, eds)),
+      [setEdges],
+    );
+ 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
-  );
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+    >
+      <Controls position="bottom-right"/>
+      {/* <MiniMap /> */}
+      <Background variant="dots" color="#6e0cab" gap={15} size={1} />
+    </ReactFlow>
+  </div>
+);
 }
