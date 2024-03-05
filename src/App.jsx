778b103149fbf87e8e1e5bcccc85992aa6a6ee37
L1MBeR1 <paol670@mail.ru>
2024-03-05 17:34:01 +0800

    Adding component panel, css , buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback,useEffect } from 'react';
+import LeftPanel from './componentPanel';
 import ReactFlow, {
 	MiniMap,
 	Controls,
@@ -9,10 +10,11 @@ import ReactFlow, {
 } from 'reactflow';
 
 import 'reactflow/dist/style.css';
+import './css/app.css';
 
 const initialNodes = [
-	{ id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-	{ id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+	{ id: '1', position: { x: 500, y: 500 }, data: { label: '1' } },
+	{ id: '2', position: { x: 500, y: 600 }, data: { label: '2' } },
 ];
 const initialEdges = [];
 
@@ -20,54 +22,57 @@ export default function App() {
 	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
 	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 	 
-	const onConnect = useCallback(
+	// const onConnect = useCallback(
 
 
-		/* Для запроса к server.js на localhost:4000/test при загрузке страницы*/
-		async (params) => {
-			try {
-				const dataToSend = { /* данные, которые нужно отправить */ };
+	// 	/* Для запроса к server.js на localhost:4000/test при загрузке страницы*/
+	// 	async (params) => {
+	// 		try {
+	// 			const dataToSend = { /* данные, которые нужно отправить */ };
 
-				const response = await fetch('http://localhost:4000/test', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify(dataToSend),
-				});
+	// 			const response = await fetch('http://localhost:4000/test', {
+	// 				method: 'POST',
+	// 				headers: {
+	// 					'Content-Type': 'application/json',
+	// 				},
+	// 				body: JSON.stringify(dataToSend),
+	// 			});
 
-				if (response.ok) {
-					const responseData = await response.json();
-					console.log('Server response:', responseData);
-				} else {
-					console.error('Error:', response.status, response.statusText);
-				}
-			} catch (error) {
-				console.error('Error sending data to the server:', error);
-			}
-		},
-		[],
-	);
+	// 			if (response.ok) {
+	// 				const responseData = await response.json();
+	// 				console.log('Server response:', responseData);
+	// 			} else {
+	// 				console.error('Error:', response.status, response.statusText);
+	// 			}
+	// 		} catch (error) {
+	// 			console.error('Error sending data to the server:', error);
+	// 		}
+	// 	},
+	// 	[],
+	// );
 
-	useEffect(() => {
-		onConnect(); 
-	}, [onConnect]);
-	/* Конец  */
+	// useEffect(() => {
+	// 	onConnect(); 
+	// }, [onConnect]);
+	// /* Конец  */
 
 
 	return (
-		<div style={{ width: '100vw', height: '100vh' }}>
-			<ReactFlow
-				nodes={nodes}
-				edges={edges}
-				onNodesChange={onNodesChange}
-				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-			>
-				<Controls position="bottom-right" />
-				{/* <MiniMap /> */}
-				<Background variant="dots" color="#6e0cab" gap={15} size={1} />
-			</ReactFlow>
+		<div className="App">
+			<LeftPanel></LeftPanel>
+			<div className='mainCanvas'>
+				<ReactFlow
+					nodes={nodes}
+					edges={edges}
+					onNodesChange={onNodesChange}
+					onEdgesChange={onEdgesChange}
+					// onConnect={onConnect}
+				>
+					<Controls position="bottom-right" />
+					{/* <MiniMap /> */}
+					<Background variant="dots" color="#6e0cab" gap={15} size={1} />
+				</ReactFlow>
+			</div>
 		</div>
 	);
 }
