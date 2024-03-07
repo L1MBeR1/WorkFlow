1d5a583401c1f9c31f09820e4da421221def6a75
dsherstyannikov <dsherstyannikov@yandex.ru>
2024-03-07 20:15:00 +0800

    added wiki

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-/*const express = require("express");
-const cors = require("cors");
-const pool = require("./database");
-const app = express();
 
-app.use(express.json());
-app.use(cors());
-
-app.post("/adduser", (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
-
-app.listen(4000, () => console.log("on L4000"));*/
 
 const express = require("express");
 const cors = require("cors");
-const pool = require("./src/database/database");
-// const { testReq } = require("./database"); 
+// const pool = require("./src/database/database");
 const routes = require("./src/routes/routes");
 const app = express();
 
@@ -24,17 +10,4 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/", routes);
-
-// app.post("/test", async (req, res) => {
-//   try {
-//     const userData = req.body;
-//     const addedUser = await testReq(userData);
-//     console.log(":", addedUser);
-//     res.json(addedUser);
-//   } catch (error) {
-//     console.error("Error adding user to the database:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
 app.listen(4000, () => console.log("on L4000"));
---
+++ src/database/routes.md
@@ -0,0 +1,9 @@
+1. # Получить все компоненты
+    localhost:4000/database/components/all
+
+
+2. # Получить функции компонента по ID компонента
+
+    localhost:4000/database/components/functions/by_component_id
+    
+    { "component_id" : 10}
