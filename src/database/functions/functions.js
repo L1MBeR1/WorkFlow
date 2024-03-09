7c138fae756fe9f26bdfc04f43cc49cfda562925
dsherstyannikov <dsherstyannikov@yandex.ru>
2024-03-09 15:10:37 +0800

    database queries no longer return empty rows

--- src/database/functions/functions.js
+++ src/database/functions/functions.js
@@ -2,9 +2,12 @@ const pool = require("../database");
 
 async function select_all_components() {
 	const query = `
-					SELECT c.id, c.name as "Название", c.description as "Описание"
-                   	FROM components.components c
-				   `;
+					SELECT DISTINCT c.id, c.name as "Название", c.description as "Описание"
+					FROM components.components c
+					JOIN components.component_function cf
+					ON c.id = cf.id_of_component
+					WHERE (cf.name is not null) AND (cf.name <> '')
+	`;
 
 	try {
 		const result = await pool.query(query);
@@ -18,10 +21,10 @@ async function select_all_components() {
 async function select_component_functions_by_component_id(component_id) {
 	const query = `
 					SELECT cf.id, cf.name as "Название"
-                    FROM components.component_function cf
-                    JOIN components.components c
-                    ON c.id = cf.id_of_component
-                    WHERE c.id = $1
+					FROM components.component_function cf
+					JOIN components.components c
+					ON c.id = cf.id_of_component
+					WHERE (c.id = $1) AND (cf.name <> '') AND (cf.name is not null)
 	`;
 
 	try {
