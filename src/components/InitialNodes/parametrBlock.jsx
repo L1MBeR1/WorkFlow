2f84517a430e2f3f9c6659228240b203b46e6614
L1MBeR1 <paol670@mail.ru>
2024-04-28 13:39:11 +0800

    Update styles for InitialNodes and FunctionBlock components; add hover and focus styles; update cursor behavior
    Changes include:
    
    * Updated background color for InitialNodes and FunctionBlock components
    * Added hover and focus styles for InitialNodes and Function

--- src/components/InitialNodes/parametrBlock.jsx
+++ src/components/InitialNodes/parametrBlock.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as Trash } from './trash.svg';
 import './initialNodes.css';
 import { useParameterBlocksData } from '../../store';
 import { v4 as uuidv4 } from 'uuid';
+import IntaractiveSection from '../mycomponent/intaractiveSection';
 
 export default memo(({ data, isConnectable }) => {
     const updateParameterBlock = useParameterBlocksData((state) => state.update);
@@ -83,37 +84,40 @@ export default memo(({ data, isConnectable }) => {
     return (
         <>
             <div>
-                <div className='parametrNode'tabIndex="0" >
+                <div className='node'tabIndex="0" >
                     {/* FIXME: текст выделяется только ПКМ*/}
                     <div contentEditable={true} onBlur={handleBlur}>
                         {header}
                     </div>
                     <hr></hr>
-                    <header >
-                        <div   className='header-name'>Название</div>
-                        <div className='header-type'>Тип</div>
-                        <div className='header-value'>Значение</div>
-                    </header>
-                    <div className='parametrs'>
-                        {parameters.map(parameter => (
-                            <div key={parameter.id} className='parameter' >
-                                <div className='parameter_name'>
-                                    <input placeholder="Имя параметра" onChange={(e) => handleNameChange(parameter.id, e.target.value)}></input>
+                    <IntaractiveSection sectionName='Параметры'>
+                        <header >
+                            <div   className='header-name'>Название</div>
+                            <div className='header-type'>Тип</div>
+                            <div className='header-value'>Значение</div>
+                        </header>
+                        <div className='parametrs'>
+                            {parameters.map(parameter => (
+                                <div key={parameter.id} className='parameter' >
+                                    <div className='parameter_name'>
+                                        <input placeholder="Имя параметра" onChange={(e) => handleNameChange(parameter.id, e.target.value)}></input>
+                                    </div>
+                                    <div className='type_value'>
+                                        <select onChange={(e) => handleTypeChange(parameter.id, e.target.value)}>
+                                            <option value="string">String</option>
+                                            <option value="number">Number</option>
+                                            <option value="boolean">Boolean</option>
+                                        </select>
+                                        <input placeholder="Значение" onChange={(e) => handleValueChange(parameter.id, e.target.value)}></input>
+                                    </div>
+                                    <div className='delete_button' onClick={() => handleDeleteParameter(parameter.id)}>
+                                        <Trash className='delete_img' />
+                                    </div>
                                 </div>
-                                <div className='type_value'>
-                                    <select onChange={(e) => handleTypeChange(parameter.id, e.target.value)}>
-                                        <option value="string">String</option>
-                                        <option value="number">Number</option>
-                                        <option value="boolean">Boolean</option>
-                                    </select>
-                                    <input placeholder="Значение" onChange={(e) => handleValueChange(parameter.id, e.target.value)}></input>
-                                </div>
-                                <div className='delete_button' onClick={() => handleDeleteParameter(parameter.id)}>
-                                    <Trash className='delete_img' />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    </IntaractiveSection>
+                    
                     <div className='addButton' onClick={addParameter}>
                         +
                     </div>
